5342653e135e277c5abb9923fda2b9294cfbd750
laon2019 <laon5987@nate.com>
2023-11-11 00:00:10 +0900

    refactor : view 파일 구조 변경

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,8 +2,8 @@ import { Console } from "@woowacourse/mission-utils";
 import { GAME_MESSAGES, ERROR_MESSAGES } from "../utils/Messages";
 import { isNumberInRange, isNumber, isNotEnglish } from "../utils/Validation";
 
-class InputView {
-    static async readDate() {
+const InputView = {
+    async readDate() {
         try {
             const input = await Console.readLineAsync(GAME_MESSAGES.INPUT_DATE);
             if(!isNumberInRange(input)) throw new Error(ERROR_MESSAGES.INVALID_RANGE);
@@ -12,9 +12,8 @@ class InputView {
         } catch (error) {
             throw new Error(ERROR_MESSAGES.DEFAULT_ERROR);
         }
-    }
-    
-    static async readMenu() {
+    },
+    async readMenu() {
         try {
             const menu = await Console.readLineAsync(GAME_MESSAGES.INPUT_MENU);
             if(!isNotEnglish(menu)) throw new Error(ERROR_MESSAGES.INVALID_ENGLISH);
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,18 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
 import { GAME_MESSAGES } from "../utils/Messages";
 
-class OutputView {
-    static printHello(){
+const OutputView = {
+    printHello(){
         Console.print(GAME_MESSAGES.HELLO_MESSAGE);
-    }
-    static printEvent(date){
+    },
+    printEvent(date){
         Console.print(`12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`);
-    }
-
+    },
     printMenu() {
         Console.print("<주문 메뉴>");
         // ...
-    }
-    // ...
+    },
 }
 export default OutputView;
