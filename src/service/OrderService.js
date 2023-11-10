fcb206d69c6b4463c5d78cb1ff4656b260b61a23
laon2019 <laon5987@nate.com>
2023-11-10 15:52:03 +0900

    feat : 입력 받은 문자열 분리

---
+++ src/service/OrderService.js
@@ -0,0 +1,14 @@
+class OrderService {
+  static splitMenuInput(menuInput) {
+    const menuItems = menuInput.split(",");
+    const parsedOrder = [];
+
+    menuItems.forEach((menuItem) => {
+      const [menuName, quantity] = menuItem.split("-");
+      parsedOrder.push([menuName, parseInt(quantity)]);
+    });
+
+    return parsedOrder;
+  }
+}
+export default OrderService;
