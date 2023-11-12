a074c43e34be73eb1449f884b2e9f032874f15ec
laon2019 <laon5987@nate.com>
2023-11-12 18:19:52 +0900

    refactor : 총혜택 금액 가격형식으로 출력

--- src/service/TotalBenefitsService.js
+++ src/service/TotalBenefitsService.js
@@ -1,7 +1,7 @@
 class TotalBenefitsService{
     calculateTotalBenefits(totalBenefits){
         if(totalBenefits>0){
-            return `-${totalBenefits}원`;
+            return `-${totalBenefits.toLocaleString()}원`;
         }
         return `${totalBenefits}원`;
     }
