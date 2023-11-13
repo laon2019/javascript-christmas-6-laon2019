d487b008a048a6a6d3b961aaf017ff90f43e1b2a
laon2019 <laon5987@nate.com>
2023-11-13 13:51:30 +0900

    refactor : calculateWeekendOrWeekdayDiscount 함수 분리

--- src/service/EventBenefitService.js
+++ src/service/EventBenefitService.js
@@ -2,51 +2,51 @@ import Menu from "../model/Menu";
 import Event from "../model/Event";
 
 class EventBenefitService {
-    #event
+  #event;
 
-    constructor() {
-        this.#event = new Event();
-    }
+  constructor() {
+    this.#event = new Event();
+  }
 
-    checkEvents(menu, date, giftMenu) {
-        const christmasDiscount = this.#calculateChristmasDiscount(date);
-        this.#event.setChristmasDiscount(christmasDiscount);
+  checkEvents(menu, date, giftMenu) {
+    const christmasDiscount = this.#calculateChristmasDiscount(date);
+    this.#event.setChristmasDiscount(christmasDiscount);
 
-        const isWeekEnd = this.#checkWeekEnd(date);
-    
-        const weekendDiscount = this.#calculateWeekendOrWeekdayDiscount(menu, isWeekEnd, Menu.getMainCourse());
-        this.#event.setWeekendDiscount(weekendDiscount);
+    const isWeekEnd = this.#checkWeekEnd(date);
 
-        const weekdayDiscount = this.#calculateWeekendOrWeekdayDiscount(menu, !isWeekEnd, Menu.getDessert());
-        this.#event.setWeekdayDiscount(weekdayDiscount);
-    
-        const specialDiscount = this.#calculateSpecialDiscount(date);
-        this.#event.setSpecialDiscount(specialDiscount);
-    
-        const benefitDiscount = this.#calculateBenefitDiscount(giftMenu);
-        this.#event.setBenefitDiscount(benefitDiscount);
+    const weekendDiscount = this.#calculateWeekendOrWeekdayDiscount(menu, isWeekEnd, Menu.getMainCourse());
+    this.#event.setWeekendDiscount(weekendDiscount);
 
-        const allEvent = this.#event.getAllEvents();
-    
-        const totalBenefitsSum = this.#calculateTotalBenefits([
-          christmasDiscount,
-          weekendDiscount,
-          weekdayDiscount,
-          specialDiscount,
-          benefitDiscount,
-        ]);
-    
-        return [allEvent, totalBenefitsSum];
-      }
+    const weekdayDiscount = this.#calculateWeekendOrWeekdayDiscount(menu, !isWeekEnd, Menu.getDessert());
+    this.#event.setWeekdayDiscount(weekdayDiscount);
+
+    const specialDiscount = this.#calculateSpecialDiscount(date);
+    this.#event.setSpecialDiscount(specialDiscount);
+
+    const benefitDiscount = this.#calculateBenefitDiscount(giftMenu);
+    this.#event.setBenefitDiscount(benefitDiscount);
+
+    const allEvent = this.#event.getAllEvents();
+
+    const totalBenefitsSum = this.#calculateTotalBenefits([
+      christmasDiscount,
+      weekendDiscount,
+      weekdayDiscount,
+      specialDiscount,
+      benefitDiscount,
+    ]);
+
+    return [allEvent, totalBenefitsSum];
+  }
 
   #calculateTotalBenefits(benefits) {
     const totalBenefitsSum = benefits.reduce((acc, value) => acc + value, 0);
     return totalBenefitsSum;
   }
 
-  #checkWeekEnd(date){
+  #checkWeekEnd(date) {
     const weekend = ["1", "2", "8", "9", "15", "16", "22", "23", "29"];
-    return weekend.includes(date)
+    return weekend.includes(date);
   }
 
   #calculateChristmasDiscount(date) {
@@ -58,29 +58,33 @@ class EventBenefitService {
     return 0;
   }
 
-  #calculateWeekendOrWeekdayDiscount(menu, isWeekEnd, menuItems) {
-    if (isWeekEnd) {
-      const count = menu.reduce((count, [itemName, quantity]) => {
-        if (menuItems.some((item) => item.name === itemName)) {
-          return count + quantity;
-        }
-        return count;
-      }, 0);
-      return count * 2023;
+  #calculateWeekendOrWeekdayDiscount(menu, isWeekend, menuItems) {
+    if (isWeekend) {
+      return this.#calculateDiscount(menu, menuItems);
     }
     return 0;
   }
 
-  #calculateSpecialDiscount(date){
+  #calculateDiscount(menu, menuItems) {
+    const count = menu.reduce((count, [itemName, quantity]) => {
+      if (menuItems.some((item) => item.name === itemName)) {
+        return count + quantity;
+      }
+      return count;
+    }, 0);
+    return count * 2023;
+  }
+
+  #calculateSpecialDiscount(date) {
     const specialDay = ["3", "10", "17", "24", "25", "31"];
     if (specialDay.includes(date)) {
-        return 1000;
+      return 1000;
     }
     return 0;
   }
-  #calculateBenefitDiscount(giftMenu){
-    if(giftMenu === "샴페인 1개"){
-        return 25000;
+  #calculateBenefitDiscount(giftMenu) {
+    if (giftMenu === "샴페인 1개") {
+      return 25000;
     }
     return 0;
   }
