f666c4fc8123d1eff875d412e59ca167c183fac8
laon2019 <laon5987@nate.com>
2023-11-13 13:47:42 +0900

    test : 주말 이벤트 테스트 생성

--- __tests__/EventTest.js
+++ __tests__/EventTest.js
@@ -14,5 +14,18 @@ describe("이벤트 테스트", () => {
       expect(totalBenefitsSum).toBe(1300);
     });
 
+    test("주말 이벤트 테스트", () => {
+        const eventBenefitService = new EventBenefitService();
+        const menu = [["티본스테이크", 2], ["타파스", 1]]; 
+        const date = "29"; 
+        const giftMenu = 0; 
+  
+        const [allEvent, totalBenefitsSum] = eventBenefitService.checkEvents(menu, date, giftMenu);
+  
+        expect(allEvent["주말 할인"]).toBe(4046);
+        expect(totalBenefitsSum).toBe(4046);
+    });
+
+    
   });
 });
