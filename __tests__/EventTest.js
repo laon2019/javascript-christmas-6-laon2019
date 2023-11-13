c62e5155578945c8582a2d839da6c80965db6121
laon2019 <laon5987@nate.com>
2023-11-13 14:11:59 +0900

    test : 이벤트 테스트 2 생성

--- __tests__/EventTest.js
+++ __tests__/EventTest.js
@@ -77,5 +77,36 @@ describe("이벤트 테스트", () => {
         expect(allEvent["증정 이벤트"]).toBe(25000);
         expect(totalBenefitsSum).toBe(31246);
     });
+    test("이벤트 테스트 1", () => {
+        const eventBenefitService = new EventBenefitService();
+        const menu = [["양송이수프", 5], ["타파스", 2], ["초코케이크", 2], ["티본스테이크", 2]]; 
+        const date = "13"; 
+        const giftMenu = true; 
+  
+        const [allEvent, totalBenefitsSum] = eventBenefitService.checkEvents(menu, date, giftMenu);
+  
+        expect(allEvent["크리스마스 디데이 할인"]).toBe(2200);
+        expect(allEvent["주말 할인"]).toBe(0);
+        expect(allEvent["평일 할인"]).toBe(4046);
+        expect(allEvent["특별 할인"]).toBe(0);
+        expect(allEvent["증정 이벤트"]).toBe(25000);
+        expect(totalBenefitsSum).toBe(31246);
+    });
+
+    test("이벤트 테스트 2", () => {
+        const eventBenefitService = new EventBenefitService();
+        const menu = [["양송이수프", 5], ["타파스", 2], ["초코케이크", 2], ["티본스테이크", 2]]; 
+        const date = "17"; 
+        const giftMenu = true; 
+  
+        const [allEvent, totalBenefitsSum] = eventBenefitService.checkEvents(menu, date, giftMenu);
+  
+        expect(allEvent["크리스마스 디데이 할인"]).toBe(2600);
+        expect(allEvent["주말 할인"]).toBe(0);
+        expect(allEvent["평일 할인"]).toBe(4046);
+        expect(allEvent["특별 할인"]).toBe(1000);
+        expect(allEvent["증정 이벤트"]).toBe(25000);
+        expect(totalBenefitsSum).toBe(32646);
+    });
   });
 });
